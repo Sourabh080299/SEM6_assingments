00b31d1430873985425580db43cba1d900cd9465
Sourabh080299 <psourabh080299@gmail.com>
2022-04-12 00:40:10 +0530

    prolog mid sem question

--- PPL/prolog_test/Q2.pl
+++ PPL/prolog_test/Q2.pl
@@ -1,8 +1,4 @@
-cube :-
-   write('Write a number: '),
-   read(Number),
-   process(Number).
-process(stop) :- !.
-process(Number) :-
-   C is Number * Number * Number,
-   write('Cube of '),write(Number),write(': '),write(C),nl, cube.
+rec([],L2,L2).
+rec(L1,[],L1) :- L1 = [_|_].
+rec([H1|T1],[H2|T2],[H1|T3]) :- rec(T1,[H2|T2],T3).
+rec([H1|T1],[H2|T2],[H2|T3]) :- rec([H1|T1],T2,T3).
