7fcadbe94e25b1c6045673e9c3dc018c37a92463
Sourabh080299 <psourabh080299@gmail.com>
2022-04-19 11:09:28 +0530

    End_sem

---
+++ PPL/End_sem_practice/Q1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+//    // int i=-1,&r=0; //invaild
+//    int *const p2 = &i2;
+//    int i;
+//    const int ic=1;
+//    i=ic;
+//    int *p1,*const p2=&ic;
+//    p1=p2;
+
+//   unsigned buf_size = 1024;
+//   int ia[buf_size];
+//   int arr[4*7 - 14];
+//    cout<<p1<<endl;
+do
+{
+	int v1, v2;
+cout<<"Please enter two numbers to sum:";
+if (cin>>v1>>v2)
+cout << "Sum is: " << v1 + v2 << endl;
+}
+while (cin);
+}
